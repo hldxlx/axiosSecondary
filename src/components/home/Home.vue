97ed5fd8e6b89ad5fcb9f1c0b7eda3b2617a92ba
binsom <18300163780@163.com>
2019-06-28 11:46:02 +0800

    first commit

---
+++ src/components/home/Home.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class='home'>
+    <home-header></home-header>
+
+    <home-swiper :swiperList='swiperList'></home-swiper>
+
+    <home-icons :iconsList='iconsList'></home-icons>
+
+    <home-location></home-location>
+    <home-activity></home-activity>
+
+    <home-hot :hotList='hotList'></home-hot>
+
+    <home-like :likeList='likeList'></home-like>
+
+    <home-vacation :vacationList='vacationList'></home-vacation>
+  </div>
+</template>
+<script>
+  import {mapState} from 'vuex'
+  import HomeHeader from './pages/Header'
+  import HomeSwiper from './pages/Swiper'
+  import HomeIcons from './pages/Icons'
+  import HomeLocation from './pages/Location'
+  import HomeActivity from './pages/Activity'
+  import HomeHot from './pages/Hot'
+  import HomeLike from './pages/Like'
+  import HomeVacation from './pages/Vacation'
+  export default {
+    components:{
+      HomeHeader,
+      HomeSwiper,
+      HomeIcons,
+      HomeLocation,
+      HomeActivity,
+      HomeHot,
+      HomeLike,
+      HomeVacation
+    },
+    data () {
+      return {
+        swiperList:[],
+        iconsList:[],
+        hotList:[],
+        likeList:[],
+        vacationList:[],
+        changeCity:''
+      }
+    },
+    computed:{
+      ...mapState(['city'])
+    },
+    methods:{
+      getHttp(){
+//        this.$http.get("/api/dataHome.json")
+//          .then((res)=>{
+//            const data = res.data.data;
+//            data.forEach((item,index)=>{
+//              if(item.city==this.city){
+//                this.swiperList=item.swiperList;
+//                this.iconsList=item.iconsList;
+//                this.hotList=item.hotList;
+//                this.likeList=item.likeList;
+//                this.vacationList=item.vacationList;
+//              }
+//            })
+//
+//          })
+
+        //利用二次封装的myGet请求
+        this.myGet("/api/dataHome.json")
+          .then((res)=>{
+             console.log(res,'===myGet拿到的res666');
+             const data = res.data;
+             data.forEach((item,index)=>{
+                if(item.city==this.city){
+                  this.swiperList=item.swiperList;
+                  this.iconsList=item.iconsList;
+                  this.hotList=item.hotList;
+                  this.likeList=item.likeList;
+                  this.vacationList=item.vacationList;
+                }
+            })
+          })
+      }
+    },
+    mounted(){
+      this.changeCity=this.city;
+      this.getHttp();
+    },
+    activated(){
+      if(this.changeCity != this.city){
+        this.getHttp();
+        this.changeCity=this.city;
+      }
+    }
+  }
+</script>
+<style scoped>
+  .home{
+    background: #f5f5f5;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,43 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+//js
+import FastClick from 'fastclick'
+FastClick.attach(document.body);
+
+//css
+import 'css/reset.css'
+import 'css/iconfont.css'
+
+
+//swiper组件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
+
+//axios
+
+import axios from 'axios'
+Vue.prototype.$http = axios
+
+import {get,post} from './utils/api'
+//将方法挂载到Vue原型上
+Vue.prototype.myGet = get;
+Vue.prototype.myPost = post;
+
+//vuex
+import store from './store'
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
